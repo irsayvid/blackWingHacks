fd1e9143a27ddc0919f24b1a00db893e59903f3c
irsayvid <dsdarimisetti@gmail.com>
2021-02-07 16:32:54 +0530

    fix styling and add survey

--- apis/survey.php
+++ apis/survey.php
@@ -5,3 +5,35 @@ $mood = $_POST['mood'];
 $bestthing = $_POST['bestthing'];
 $worstthing = $_POST['worstthing'];
 $reflections = $_POST['reflections'];
+
+if(isset($date) && isset($mood) && isset($bestthing) && isset($worstthing) && isset($reflections))
+{
+    if($mood == "Sad")
+    {
+
+    }
+    else if($mood == "Stressed")
+    {
+
+    }
+    else if($mood == "Tired")
+    {
+
+    }
+    else if($mood == "Angry")
+    {
+
+    }
+    else if($mood == "Okay")
+    {
+
+    }
+    else if($mood == "Excited")
+    {
+
+    }
+    else if($mood == "Happy")
+    {
+
+    }
+}
